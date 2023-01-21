578f8a03698d30fbaff2b662bf6a23cb47f220f4
Rifqi Pratama <rifqipratama9@gmail.com>
2023-01-21 20:19:36 +0700

    install react-hook-fom, add dashboard page with several components

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,36 @@
 import Head from 'next/head';
-import { Button, Heading, Text, Code } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 
 import { useAuth } from '@/lib/auth';
+import Logo from '@/components/Logo';
 
 const Home = () => {
   const auth = useAuth();
 
   return (
-    <div className="container">
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       <Head>
         <title>Fast Feedback</title>
       </Head>
-      <main>
-        <Heading>Fast Feedback</Heading>
-        <Text>
-          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
-        </Text>
-        {auth.user ? (
-          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
-        ) : (
-          <Button onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
-        )}
-      </main>
-    </div>
+
+      <Logo color="black" name="logo" boxSize="64px" />
+
+      {auth.user ? (
+        <Button as="a" href="/dashboard">
+          View Dashboard
+        </Button>
+      ) : (
+        <Button mt={4} size="sm" onClick={(e) => auth.signinWithGitHub()}>
+          Sign In
+        </Button>
+      )}
+    </Flex>
   );
 };
 
